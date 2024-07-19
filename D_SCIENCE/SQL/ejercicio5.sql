03c257c7f3cecea4d3f84f9b605fd03f940737d3
TheGuzman <guzmanmedrano.diego@gmail.com>
2024-07-19 15:50:46 +0200

    feat(sql): add exercise 6

---
+++ D_SCIENCE/SQL/ejercicio5.sql
@@ -0,0 +1,8 @@
+'Escribe una consulta que te muestre la información detallada 
+de los tickets que han comprado las personas que se llaman Irina.
+Tip: Si quieres ver un ejemplo del operador necesario:'
+
+
+SELECT *
+FROM tickets
+WHERE passenger_name LIKE 'IRINA%'
