e1b4b2dc59c95a392386be5b6edc307ca45a00c5
TheGuzman <guzmanmedrano.diego@gmail.com>
2025-06-08 21:51:11 +0200

    fix(test)

--- D_SCIENCE/PYTHON/test/test_ejercicio_11.py
+++ D_SCIENCE/PYTHON/test/test_ejercicio_11.py
@@ -7,9 +7,9 @@ from ejercicio_11 import calculate_age
 class TestCalculateAge(unittest.TestCase):
     
     def test_calculate_age(self):
-        self.assertEqual(calculate_age('31/01/1994'),30)
-        self.assertEqual(calculate_age('10/09/1987'),37)
-        self.assertEqual(calculate_age('10/09/1982'),42)
+        self.assertEqual(calculate_age('31/01/1994'),31)
+        self.assertEqual(calculate_age('10/09/1987'),38)
+        self.assertEqual(calculate_age('10/09/1982'),43)
 
 
 if __name__=='__main__':
